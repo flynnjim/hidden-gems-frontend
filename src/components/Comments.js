b5a3a62bbcbc553fb429a4e24eb0534a2a74b4c7
Rambo7X7 <hassanchudary7x7@gmail.com>
2024-10-03 16:52:24 +0100

    comments update 1.2

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -6,13 +6,28 @@ import CommentForm from "./CommentForm";
 
 function Comments({ gem_id }) {
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     //change id dynamically
-    getCommentsByGemId(2).then((response) => {
-      setComments(response);
-    });
+    getCommentsByGemId(2)
+      .then((response) => {
+        setComments(response);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setIsError(true);
+      });
   }, []); //add gem_id to dependencies array
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
+  if (isError) {
+    return <p>Something went worng</p>;
+  }
 
   return (
     <>
