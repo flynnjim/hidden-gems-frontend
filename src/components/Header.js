1e4c14da8efb7e5dd919cbefef8f9541840ed7ab
Olamide Olawuyi <christianaao@ChristianaO-Laptop>
2024-10-05 14:45:16 +0100

    progress on post gem component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ import NavBar from "./NavBar";
 function Header() {
   return (
     <header>
-      <h1 className="container">HIDDEN GEMS </h1>
-      <NavBar/>
+      <div className="container">
+        <h1>HIDDEN GEMS</h1>
+        <NavBar/>
+      </div>
     </header>
   );
 }
