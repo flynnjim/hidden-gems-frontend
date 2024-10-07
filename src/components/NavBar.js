0fa8c297af50b36630bff5c260fb49d17afe3d68
Rambo7X7 <hassanchudary7x7@gmail.com>
2024-10-07 11:33:09 +0100

    Add Login to menu bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,4 @@
-import {
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/react";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -36,14 +32,25 @@ function NavBar() {
         onClose={() => setIsOpen(false)}
         className={`flex flex-col justify-center items-center`}
       >
-      <div className="fixed inset-0 flex w-screen items-center justify-start z-index-[2000] bg-[#00000040]">
+        <div className="fixed inset-0 flex w-screen items-center justify-start z-index-[2000] bg-[#00000040]">
           <DialogPanel className="max-w-lg space-y-4 border bg-white text-black p-12 w-[300px] h-[100vh]">
             <DialogTitle className="font-bold">MENU</DialogTitle>
             <ul>
-              <li><a href="/">HOME</a></li>
-              <li><a href="/gems">ALL GEMS</a></li>
-              <li><a>MY ACCOUNT</a></li>
-              <li><a href="/add-gem">ADD A GEM</a></li>
+              <li>
+                <a href="/">HOME</a>
+              </li>
+              <li>
+                <a href="/login">LOGIN</a>
+              </li>
+              <li>
+                <a href="/gems">ALL GEMS</a>
+              </li>
+              <li>
+                <a>MY ACCOUNT</a>
+              </li>
+              <li>
+                <a href="/add-gem">ADD A GEM</a>
+              </li>
               <li>SIGN OUT</li>
             </ul>
             <div className="flex gap-4">
